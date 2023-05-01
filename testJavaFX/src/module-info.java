98e3a4290ca400340359b0be16c0e0f7343c3faa
gudrn4616 <128803700+gudrn4616@users.noreply.github.com>
2023-05-01 15:54:45 +0900

    0.1

--- testJavaFX/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module testJavaFX {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.base;
-	requires java.desktop;
-	requires javafx.graphics;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
